096dfe164d92a6380177eeb6fbec977ca24f4e27
Manan Jain <manan09mj@gmail.com>
2021-02-02 16:43:27 +0530

    index.js
        Add messages array containing objects {text:<message>, user:<name>, added:<datetime>}
    
        Add a get router on "/" which will show the home page
    
        Add a get router on "/new" which will show the form to add a new msg board
    
        Add a post router on "/new" which will take the form data into messages array and then redirect
    the res to "/"

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+/* Messages */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date().toLocaleString(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date().toLocaleString(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message", messages: messages });
+});
+
+/* Get new page */
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Message" });
+});
+
+/* Post new page */
+router.post("/new", function (req, res, next) {
+  messages.push({
+    user: req.body.name,
+    text: req.body.message,
+    added: new Date().toLocaleString(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
